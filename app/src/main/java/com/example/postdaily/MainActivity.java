0b696b0b3e138d1da572e3540ef334a7f96a4980
[MARIAISLAMROTHNA] <[mariaislamofficial@gmail.com]>
2020-11-28 21:17:10 +0600

    Navigation drawer added

--- app/src/main/java/com/example/postdaily/MainActivity.java
+++ app/src/main/java/com/example/postdaily/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.postdaily;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,21 +26,85 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference RootRef;
     private String currentUserID;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout drawerLayout;
+    private RecyclerView postList;
+    private Toolbar mToolbar;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mToolbar=(Toolbar)findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Home");
+
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawable_layout);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.drawer_open,R.string.drawer_close );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView=(NavigationView)findViewById(R.id.navigation_view);
+        View navview=navigationView.inflateHeaderView(R.layout.navigation_header);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                UserMenuSelector(item);
+                return false;
+            }
+        });
+
+
         mAuth = FirebaseAuth.getInstance();
 
         RootRef = FirebaseDatabase.getInstance().getReference();
 
-        Toolbar mToolbar = findViewById(R.id.main_page_toolbar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("POST_DAILY");
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+       if(actionBarDrawerToggle.onOptionsItemSelected(item))
+       {
+           return true;
+       }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void UserMenuSelector(MenuItem item) {
+   switch (item.getItemId())
+   {
+       case R.id.nav_home:
+           Toast.makeText(MainActivity.this,"HomeChecked",Toast.LENGTH_LONG).show();
+           break;
+       case R.id.nav_profile:
+           Toast.makeText(MainActivity.this,"profileChecked",Toast.LENGTH_LONG).show();
+           break;
+       case R.id.nav_post:
+           Toast.makeText(MainActivity.this,"Add A Post Checked",Toast.LENGTH_LONG).show();
+           break;
+       case R.id.nav_friends:
+           Toast.makeText(MainActivity.this,"FriendsChecked",Toast.LENGTH_LONG).show();
+           break;
+       case R.id.nav_find_friendst:
+           Toast.makeText(MainActivity.this,"Find Friends",Toast.LENGTH_LONG).show();
+           break;
+       case R.id.nav_logout:
+           Toast.makeText(MainActivity.this,"Logout",Toast.LENGTH_LONG).show();
+           break;
+
+
+   }
     }
 
     @Override
